d9bd9518f972cef1c65edff781264946043accb3
NicolasDubrayNBI-H <nicolas.dubray@student.nbi-handelsakademin.se>
2025-09-14 21:22:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AppForWeek2TuesdayExercises1And2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
